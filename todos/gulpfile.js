0cf6b670beea3709687de56c0aac6c8b4542da33
Tuan Huynh <tuanlh@gmail.com>
2014-03-22 01:18:27 -0700

    View README.md for quick start guide
    Replaced starter kit markup with markup for todos

--- todos/gulpfile.js
+++ todos/gulpfile.js
@@ -58,6 +58,6 @@ gulp.task('default', function() {
   // place code for your default task here
   startExpress();
   startLivereload();
-  gulp.watch('src/**/*', notifyLivereload);
+  gulp.watch('./src/**/*', notifyLivereload);
   console.log('gulp and running');
 });
