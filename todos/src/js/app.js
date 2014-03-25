d779505228d7c402500310aa949ed9e36373f369
Tuan Huynh <tuanlh@gmail.com>
2014-03-25 02:22:55 -0700

    Add data model

--- todos/src/js/app.js
+++ todos/src/js/app.js
@@ -1,11 +1,45 @@
-App = Ember.Application.create();
+// -- Application --------------------------------------------------------------
+Todos = Ember.Application.create();
 
-App.Router.map(function() {
-  // put your routes here
+Todos.ApplicationAdapter = DS.FixtureAdapter.extend();
+
+// -- Router -------------------------------------------------------------------
+Todos.Router.map(function() {
+  this.resource('todos', { path: '/' });
 });
 
-App.IndexRoute = Ember.Route.extend({
+Todos.TodosRoute = Ember.Route.extend({
   model: function() {
-    return ['red', 'yellow', 'blue'];
+    return this.store.find('todo');
   }
 });
+
+// Todo.IndexRoute = Ember.Route.extend({
+//   model: function() {
+//     return "Hello World!";
+//   }
+// });
+
+// -- Models -------------------------------------------------------------------
+Todos.Todo = DS.Model.extend({
+  title: DS.attr('string'),
+  isCompleted: DS.attr('boolean')
+});
+
+Todos.Todo.FIXTURES = [
+ {
+   id: 1,
+   title: 'Learn Ember.js!',
+   isCompleted: true
+ },
+ {
+   id: 2,
+   title: '...',
+   isCompleted: false
+ },
+ {
+   id: 3,
+   title: 'Profit!',
+   isCompleted: false
+ }
+];
