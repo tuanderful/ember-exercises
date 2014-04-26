291daef68954c7aafcbabe73f9c6ffe683a8de2f
Tuan Huynh <tuanlh@gmail.com>
2014-04-25 23:13:05 -0700

    Finished Demo

--- todos/src/js/app.js
+++ todos/src/js/app.js
@@ -5,12 +5,41 @@ Todos.ApplicationAdapter = DS.FixtureAdapter.extend();
 
 // -- Router -------------------------------------------------------------------
 Todos.Router.map(function() {
-  this.resource('todos', { path: '/' });
+  this.resource('todos', { path: '/' }, function(){
+    this.route('active');
+    this.route('completed');
+  });
 });
 
 Todos.TodosRoute = Ember.Route.extend({
   model: function() {
     return this.store.find('todo');
+  }
+});
+
+Todos.TodosIndexRoute = Ember.Route.extend({
+  setupController: function () {
+    this.controllerFor('todos').set('filteredTodos', this.modelFor('todos'));
+  }
+});
+
+Todos.TodosActiveRoute = Ember.Route.extend({
+  setupController: function () {
+    var todos = this.store.filter('todo', function (todo) {
+      return !todo.get('isCompleted');
+    });
+
+    this.controllerFor('todos').set('filteredTodos', todos);
+  }
+});
+
+Todos.TodosCompletedRoute = Ember.Route.extend({
+  setupController: function () {
+    var todos = this.store.filter('todo', function (todo) {
+      return todo.get('isCompleted');
+    });
+
+    this.controllerFor('todos').set('filteredTodos', todos);
   }
 });
 
